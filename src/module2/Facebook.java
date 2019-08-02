64dbe5560d222a820e68f2d36679ecf6b04b5c95
anjana <anju_arzoo@yahoo.co.in>
2019-08-02 13:38:46 -0400

    first commit

---
+++ src/module2/Facebook.java
@@ -0,0 +1,24 @@
+package module2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Facebook {
+
+	private static final String driver.get = null;
+	public static void main(String[] args) {
+		// instantiate chrome browser
+		System.setProperty("webdriver chromedriver", "C:\\Selenium Training\\Installation stuff\\Drivervalue");
+
+		WebDriver driver = new  ChromeDriver();
+	}
+       //open the AUT
+	
+	{
+		 driver.get("https;//www.facebook.com/");
+		 //locator
+		 //ID
+		 
+	}
+	
+	}
---
+++ src/module2/basicSeleniumcommands.java
@@ -0,0 +1,48 @@
+package module2;
+
+public class basicSeleniumcommands {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// single line comment
+		/*
+		 * multi line comment
+		 */
+		
+		//Initialize/instantiate/open browser
+		//chrome browser
+		system.Setproperty("webdriver.chrome.driver","C:\\Selenium Training\\Installation stuff\\Driver")
+		webdriver driver = new chromedriver();
+		
+		//close and quit command
+		driver.close();//this will close the current active window
+		driver.quit();// this will close all the window opened during this instance
+		
+		//get command
+		driver.get("https://www.eudreka.co")//to open URL of AUT
+		driver.gettitle();//to fetch the title of the page and use it either for validation
+		driver.getcurrentURL();//to fetch the current URL of the AUT
+		
+		//navigation command
+		//move to the next page
+		driver.navigation().forward();
+		//move to previous page
+		driver.navigation().back();
+		//page refresh
+		driver.navigate().refresh();
+		//element handling 
+		//click
+		driver.FindElement(by.id("")).click();
+		//type
+		driver.findElement(by.id("").sendkeys("anjana")
+		//clear command
+		driver.findElement(by.id("").clear();
+				
+		
+		
+		
+		
+	}
+
+}
